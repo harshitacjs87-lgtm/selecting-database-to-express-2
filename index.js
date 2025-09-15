11f5e76b398510802b3870cb4a15bf3846f0bf96
harshitacjs87-lgtm <your_github_email@example.com>
2025-09-15 14:10:33 +0530

    database to express

--- index.js
+++ index.js
@@ -1,15 +1,30 @@
 const express = require('express');
-const { resolve } = require('path');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 const app = express();
-const port = 3010;
-
 app.use(express.static('static'));
 
-app.get('/', (req, res) => {
-  res.sendFile(resolve(__dirname, 'pages/index.html'));
+const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/studentProjectsDB';
+const port = process.env.PORT || 3000;
+
+mongoose.connect(MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+
+
+.then(() => {
+  console.log('Connected to MongoDB');
+})
+
+.catch((err) => {
+  console.error('Error connecting to MongoDB:', err);
 });
 
+
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`Server listening on port ${port}`);
 });
